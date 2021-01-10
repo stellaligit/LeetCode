03120f000ea106977600c4260817266a02c6b449
stellaligit <stellaligit@users.noreply.github.com>
2021-01-10 11:53:58 -0500

    Update RemoveElement (most likely the last time)
    Very simple :)

--- Easy/RemoveElement.py
+++ Easy/RemoveElement.py
@@ -3,10 +3,10 @@
 class Solution:
     def removeElement(self, nums: [], val: int) -> int:
         i = -1
-        for j in range(len(nums)):
-            if nums[j] != val:
+        for j in nums:
+            if j != val:
                 i += 1
-                nums[i] = nums[j]
+                nums[i] = j
 
         return i + 1
 

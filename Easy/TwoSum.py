cdee9e1bcd82fdfc37e56843a05d41bfb7e5eba4
stellaligit <stellaligit@users.noreply.github.com>
2021-01-12 19:24:15 -0500

    Update TwoSums
    Oh, heh... heh... heh... I forgot to submit this a loooong time ago lol. Thank you.

--- Easy/TwoSum.py
+++ Easy/TwoSum.py
@@ -1,8 +1,7 @@
 # https://leetcode.com/problems/two-sum/
 
-# solution 1:
-class Solution1:
-    def twoSum(self, nums: List[int], target: int) -> List[int]:
+class Solution:
+    def twoSum1(self, nums: [], target: int) -> []:
         a = len(nums)
         for i in range(a):
             d = target - nums[i]
@@ -10,13 +9,55 @@ class Solution1:
                 if d == nums[j]:
                     return [i, j]
 
-# solution 2:
-
-
-class Solution2:
-    def twoSum(self, nums: List[int], target: int) -> List[int]:
+    def twoSum2(self, nums: [], target: int) -> []:
         a = len(nums)
         for i in range(a):
             for j in range(i+1, a):
                 if (nums[i] + nums[j]) == target:
                     return [i, j]
+
+    def twoSum3(self, nums: [], target: int) -> []:
+        m = {}
+        a = len(nums)
+        for i in range(a):
+            if nums[i] in m:
+                return [m[nums[i]], i]
+            else:
+                m[target-nums[i]] = i
+
+
+if __name__ == "__main__":
+    try:
+        test = Solution()
+        assert test.twoSum1([2, 7, 11, 15], 9) == [0, 1]
+        assert test.twoSum2([2, 7, 11, 15], 9) == [0, 1]
+        assert test.twoSum3([2, 7, 11, 15], 9) == [0, 1]
+        print("Good job :)")
+
+        import time
+        import random
+
+        print("create a really large list")
+        start_time = time.time()
+        nums = list(range(1, 10001))
+        random.shuffle(nums)
+        print(time.time() - start_time)
+        print("init done. start to test.\r\n\r\n")
+
+        start_time = time.time()
+        res = test.twoSum1(nums, 19999)
+        t = time.time() - start_time
+        print("Test 1 ans:", res, "time:", t)
+
+        start_time = time.time()
+        res = test.twoSum2(nums, 19999)
+        t = time.time() - start_time
+        print("Test 2 ans:", res, "time:", t)
+
+        start_time = time.time()
+        res = test.twoSum3(nums, 19999)
+        t = time.time() - start_time
+        print("Test 3 ans:", res, "time:", t)
+
+    except AssertionError:
+        print("Error! Error! Oh no! Drumroll please! Don don don!!!")

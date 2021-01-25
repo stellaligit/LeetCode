cf9eb5eb3804c418d6346e7823777d826d62408f
stellaligit <68718208+stellaligit@users.noreply.github.com>
2021-01-24 22:04:50 -0500

    Update SearchInsertPosition.py
    Another solution lol. Maybe it's a better solution idk. MerCi bEauCouP (thank you very much in French).

--- Easy/SearchInsertPosition.py
+++ Easy/SearchInsertPosition.py
@@ -3,23 +3,18 @@
 class Solution:
     def searchInsert(self, nums: [], target: int) -> int:
         # if target not in nums:
-        flag = False
-        for i in nums:
-            if i == target:
-                flag = True
-
-        if not flag:
-            for i in range(len(nums)):
-                if nums[-1] < target:
-                    return len(nums)
-                elif nums[0] > target:
-                    return 0
-                elif nums[i] < target and nums[i + 1] > target:
-                    return i + 1
-
         for i in range(len(nums)):
             if nums[i] == target:
                 return i
+                flag = True
+
+        for i in range(len(nums)):
+            if nums[-1] < target:
+                return len(nums)
+            elif nums[0] > target:
+                return 0
+            elif nums[i] < target and nums[i + 1] > target:
+                return i + 1
 
 
 if __name__ == "__main__":
